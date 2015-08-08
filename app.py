610407ebc4c7ac9934f7fbfc0d341678d4409861
Ashu Desai <ashuddesai@gmail.com>
2015-08-08 01:35:22 -0400

    fixing plist url

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ def authenticate(f):
 
 @app.route('/')
 def base():
-  url = 'itms-services://?action=download-manifest&url=' + urllib.quote('https://jkbx.es/static/jukebox.plist')
+  url = 'itms-services://?action=download-manifest&url=' + urllib.quote('https://www.jkbx.es/static/jukebox.plist')
   pic = 'https://s3.amazonaws.com/mgwu-misc/jukebox/jukebox.png'
   return render_template('download.html', title='Jukebox', link=url, picture=pic)
 
